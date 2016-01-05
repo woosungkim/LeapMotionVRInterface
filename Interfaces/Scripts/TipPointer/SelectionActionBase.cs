0f27e0637ae2938849021d2a32818b0dad1fc87d
woosungkim <loftyws91@naver.com>
2016-01-05 10:43:23 +0900

    add camera transitions interface

---
+++ Interfaces/Scripts/TipPointer/SelectionActionBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionActionBase : MonoBehaviour {
+
+	public string name;
+	
+	public void doAction()
+	{
+		Debug.Log ("clicked item name : " + name);
+
+	}
+}

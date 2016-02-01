4227e1af4d36e2988bf7398a074d071dc1aeb5ac
Jung HeeChang <jgmlckd@gmail.com>
2016-02-01 16:11:00 +0900

    Update CameraTrasition

---
+++ Interfaces/Scripts/CameraTransition/PlanController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+public class PlanController: MonoBehaviour
+{
+
+    WebCamTexture wtex;
+    Texture2D boxTex;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        print("start");
+        print(this.GetComponent<Renderer>().material);
+        print(WebCamTexture.devices.Length);
+
+
+        //캡처가 저장될 cube Object
+        GameObject box = GameObject.Find("Plane");
+        //캡처를 할 실제 텍스쳐 생성과 적용
+        boxTex = new Texture2D(640, 480, TextureFormat.ARGB32, false);
+        box.GetComponent<Renderer>().material.mainTexture = boxTex;
+
+        if (WebCamTexture.devices.Length < 1)
+            return;
+
+        wtex = new WebCamTexture(WebCamTexture.devices[0].name, 640, 480, 30);
+        this.GetComponent<Renderer>().material.mainTexture = wtex;
+        wtex.Play();
+    }
+
+    void OnGUI()
+    {
+        /*
+        if (GUI.Button(new Rect(100, 100, 200, 200), "촬영"))
+        {
+            saveTex();
+        }
+         */ 
+    }
+
+    void saveTex()
+    {
+        print(wtex.width + " " + wtex.height + " " + wtex.GetPixels().Length);
+        boxTex.SetPixels(wtex.GetPixels());
+        //중요
+        boxTex.Apply();
+
+        //이미지의 저장
+        byte[] savedata = boxTex.EncodeToPNG();
+        FileStream fs = System.IO.File.Create(@"C:\Apache24\htdocs\uploads\unitytest.png");
+        fs.Write(savedata, 0, savedata.Length);
+        fs.Close();
+
+    }
+
+    // Update is called once per frame
+    Vector3 rotVec = new Vector3(0, 1, 0);
+    void Update()
+    {
+        //this.transform.Rotate(rotVec);
+    }
+}
---
+++ Interfaces/Scripts/CameraTransition/SwitcherHDCamera.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitcherHDCamera : MonoBehaviour {
+
+	private static SwitcherHDCamera _instance;
+    
+	enum CameraState : int { AR, VR };
+
+	public GameObject _target = null;
+	private GameObject plane;
+    private CameraState state;
+	private Vector3 vrPos, arPos;
+    private Vector3 tempPos;
+    private bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+		if (_target == null) {
+			Debug.Log ("no target");
+		}
+
+        state = CameraState.VR;
+        arPos = new Vector3(_target.transform.position.x, _target.transform.position.y , _target.transform.transform.position.z + 0.5f);
+        vrPos = new Vector3(_target.transform.position.x, _target.transform.position.y - 0.8f, _target.transform.transform.position.z + 0.5f);
+        plane = GameObject.Find ("Plane");
+        plane.transform.position = vrPos;
+        plane.GetComponent<Renderer>().enabled = true;
+	}
+
+	void Awake() {
+		_instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_target != null) {
+			// track camera position & rotation
+            transform.position =  Vector3.Lerp( _target.transform.position - (_target.transform.forward * 0.1f),transform.position, 5f * Time.deltaTime);
+            transform.rotation = _target.transform.rotation;
+            if(state == CameraState.AR)
+            {
+                tempPos = plane.transform.position;
+            }
+            else
+            {
+                tempPos = new Vector3(plane.transform.position.x, plane.transform.position.y + 0.8f, plane.transform.position.z);
+            }
+
+            arPos = tempPos;
+            vrPos = new Vector3(tempPos.x, tempPos.y - 0.8f, tempPos.z);
+            
+           
+            
+		}
+	}
+
+    public static SwitcherHDCamera GetInstance()
+    {
+        return _instance;
+    }
+    
+	public void switchCamera() {
+		Debug.Log ("switch camera");
+		StartCoroutine (switchCameraRutine( state ));
+
+	}
+    
+	public void switchCameraToVR() {
+        if(!isPlaying && this.state!=CameraState.VR)
+        {
+            isPlaying = true;
+            StartCoroutine(switchCameraRutine(CameraState.AR));
+        }
+		    
+	}
+
+	public void switchCameraToAR() {
+        if(!isPlaying && this.state!=CameraState.AR)
+        {
+            isPlaying = true;
+            StartCoroutine(switchCameraRutine(CameraState.VR));
+        }
+		    
+	}
+
+	IEnumerator switchCameraRutine( CameraState from ) {
+		float time = 0.0f;
+		Vector3 startPos, endPos;
+
+        startPos = plane.transform.position;
+		endPos = (from == CameraState.AR) ? vrPos : arPos;
+
+		while (time < 1.0f) {
+            plane.transform.position = Vector3.Lerp(startPos, endPos, time);
+
+			time = time + 0.1f;
+			yield return new WaitForSeconds(0.02f);
+		}
+
+		if (from == CameraState.VR) {
+			Debug.Log("change camera to AR");
+            isPlaying = false;
+			state = CameraState.AR;
+			
+		} else if (from == CameraState.AR) {
+			Debug.Log("change camera to VR");
+            isPlaying = false;
+			state = CameraState.VR;
+		}
+
+		yield return 0;
+	}
+}

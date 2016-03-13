d4709f66ac5957c52938531f85906666ddc53704
woosungkim <loftyws91@naver.com>
2016-03-13 20:04:18 +0900

    PointerHandModelSync update

---
+++ Interfaces/Scripts/TipPointer/PointerHandModelSync.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerHandModelSync : MonoBehaviour {
+
+	private Camera _Camera;
+
+	// Use this for initialization
+	void Start () {
+		_Camera = Camera.main;
+
+		gameObject.transform.SetParent (_Camera.transform, false);
+
+		gameObject.transform.eulerAngles = new Vector3 (270.0f, 180.0f, 0.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
